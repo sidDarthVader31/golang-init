7af7e7c6f5c631521ac7ed588d1ab778b9116980
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-01-04 14:44:53 +0530

    common datatypes of go

--- app_structure/main.go
+++ app_structure/main.go
@@ -1,7 +1,5 @@
-// a package clause starts every source file
-// main is a special name declaring an executable rather than a library package
+// a package clause starts every source file main is a special name declaring an executable rather than a library package package main
 package main
-
 import "fmt"
 
 const secondsInhour = 3600
@@ -22,5 +20,48 @@ func main() {
   const (min1 = 100
     min2 =200
     min3 ="sid" )
+
   fmt.Println(min1, min2, min3)
+
+  //arrays
+  var numbers = [4] int {1,2,3,4}
+  fmt.Printf("numbers array : %T \n",numbers)
+
+  //slices
+  var slice =[]string{"london","paris","milan","madrid"}
+  fmt.Printf("slice:%T \n", slice)
+
+
+  //maps
+  data := map[string]float64{
+    "key1": 1.4,
+    "key2": 2.4,
+  }
+  fmt.Printf("maptype: %T\n", data);
+
+  //struct
+
+  type person struct{
+    name string
+    age int
+    designation string
+  }
+  //var newUser Person;
+  //fmt.Printf("%T", newUser);
+  
+  //pointers
+  var x int = 2
+  var xAddress *int = &x
+  fmt.Printf("type of xAddress: %T , and value is %v", xAddress, xAddress);
+
+
+
+
+
+
+
+
+
+
+
 }
