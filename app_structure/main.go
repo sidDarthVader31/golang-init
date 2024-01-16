42791535318213475f2b78f71b64f7a76fe43970
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-01-16 19:31:13 +0530

    add strings, runes and bytes

--- app_structure/main.go
+++ app_structure/main.go
@@ -243,10 +243,73 @@ fmt.Println(keyedArray)
   fmt.Println(s4) // prints [1000,30]
   fmt.Println(s1) // prints [10,1000,30,40,50,60]
 
+  //slices with arrays 
+  arr := [4] int {10,20,30,40}
+  sarr1, sarr2 := arr[0:2], arr[1:3]
+  fmt.Println(arr, sarr1, sarr2)
+  //modifying arr 
+  arr[1] = 3 // this will modify the slices as well since arr is their backing array 
+  fmt.Println(arr, sarr1, sarr2);
+  sarr1[1] = 1000 // this wil also modify the slices and array arr 
+  fmt.Println("modifying sarr1")
+  fmt.Println(arr, sarr1, sarr2)
 
+  // if we want to copy a slice from another slice we can use append if we do not want slices to share 
+  // the same backing array 
 
+  cars := []string {"Tata","Mahindra", "Kia", "Hyundai"}
+  newCars := []string {}
+  newCars= append(newCars, cars...)
+  fmt.Println("cars:", cars, newCars)
 
+  cars [0] = "Honda" // this will not modify newCars 
+  fmt.Println(cars, newCars)
 
+  //stringsk bytes and runes
+
+    // characters or rune literals are expressed in Go by enclosing them in single quotes
+    // declaring a variable of type rune (alias to int32)
+    var1 := 'a'
+    fmt.Printf("Type: %T, Value:%d \n", var1, var1) // => Type: int32, Value:97
+    // value is 97 (the code point for a)
+ 
+    // declaring a string value that contains non-ascii characters
+    str := "ţară" // ţară means country in Romanian
+    // 't', 'a' ,'r' and 'a' are runes and each rune occupies beetween 1 and 4 bytes.
+ 
+    //The len() built-in function returns the no. of bytes not runes or chars.
+    fmt.Println(len(str)) // -> 6,  4 runes in the string but the length is 6
+ 
+    // returning the number of runes in the string
+    m := utf8.RuneCountInString(str)
+    fmt.Println(m) // => 4
+ 
+    // by using indexes we get the byte at that positioin, not rune.
+    fmt.Println("Byte (not rune) at position 1:", str[1]) // -> 163
+ 
+    // decoding a string byte by byte
+    for i := 0; i < len(str); i++ {
+        fmt.Printf("%c", str[i]) // -> Å£arÄ
+    }
+ 
+    fmt.Println("\n" + strings.Repeat("#", 10))
+ 
+    // decoding a string rune by rune manually:
+    for i := 0; i < len(str); {
+        r, size := utf8.DecodeRuneInString(str[i:]) // it returns the rune in string in variable r
+        //and its size in bytes in variable size
+ 
+        // printing out each rune
+        fmt.Printf("%c", r) // -> ţară
+        i += size           // incrementing i by the size of the rune in bytes
+    }
+ 
+    fmt.Println("\n" + strings.Repeat("#", 10))
+ 
+    // decoding a string rune by rune automatically:
+    for i, r := range str { //the first value returned by range is the index of the byte in string where rune starts
+        fmt.Printf("%d -> %c", i, r) // => ţară
+    }
 
 
 }
