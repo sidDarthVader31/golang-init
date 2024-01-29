fe3b446cdf18cbb358c1d414a984a1857d45f27d
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-01-29 18:08:10 +0530

    fix: datarace condition with mutex

--- datarace/main.go
+++ datarace/main.go
@@ -12,14 +12,20 @@ func main(){
   gr := 100;
   wg.Add(gr*2)
 
+  //fixing datarace with mutex 
+  var mx sync.Mutex
   for i:=0;i<100;i++{
     go func() {
       time.Sleep(time.Second /10)
+      mx.Lock()
+      defer mx.Unlock()
       n++
       wg.Done()
     }()
     go func() {
       time.Sleep(time.Second /10)
+      mx.Lock()
+      defer mx.Unlock()
       n--;
       wg.Done()
     }()
